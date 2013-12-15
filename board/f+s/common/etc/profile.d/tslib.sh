f85f1fdb73d7f5e462371bc7b10a0c9d813c1bef
Hartmut Keller <keller@fs-net.de>
2013-12-15 14:41:52 +0100

    Improve F&S X-Server start script S35x11
    - Don't guess the correct /dev/input/event number. This never worked
      satisfyingly. Now use the event given in /etc/X11/tslib-event.conf,
      or if this does not exist simply use event0 by default.
    - Start X-Server on VT02. This makes restarting from the command line
      possible.
    - Add a third event in etc/X11/xorg.conf, we might have more than
      two sources.
    - Add comments to etc/profile.d/tslib.sh.

--- board/f+s/common/etc/profile.d/tslib.sh
+++ board/f+s/common/etc/profile.d/tslib.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# F&S X-Server touch configuration
 
 tsconf_file=/var/run/tslib.conf
 
-if [ -e $tsconf_file ]
+# If there is the configuration file, source it (see /etc/init.d/S35x11).
+# This makes ts_calibrate and ts_test work right away without the need to set
+# anything manually
+if [ -f $tsconf_file ]
 then
 	. $tsconf_file
 fi

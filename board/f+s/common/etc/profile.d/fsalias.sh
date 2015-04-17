f4ba3ab219866d751c042449f7d36dc76ad761ba
Hartmut Keller <keller@fs-net.de>
2015-04-17 11:10:43 +0200

    Add some useful aliases as /etc/profile.d/fsalias.sh
    When working on the board, some useful command aliases were missing.
    Add these by adding a file /etc/profile.d/fsalias.sh. This file is
    sourced into all interactive shell invocations.
    
    The following aliases are added:
    
    alias cp="cp -i"
    alias mv="mv -i"
    alias rm="rm -i"
    alias Rm="rm -f"
    alias l="ls -lF"
    alias ll="ls -lF"
    alias ls="ls -F"
    alias ..="cd .."
    alias pd='pushd'

---
+++ board/f+s/common/etc/profile.d/fsalias.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# F&S additional and useful aliases
+
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+alias Rm="rm -f"
+alias l="ls -lF"
+alias ll="ls -lF"
+alias ls="ls -F"
+alias ..="cd .."
+alias pd='pushd'

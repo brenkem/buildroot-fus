7da292d17aa3c0758549400ab403d2a40c01c5fa
Hartmut Keller <keller@fs-net.de>
2016-06-13 17:30:37 +0200

    Add imx-gst1-plugin to gstreamer1 package
    - Add imx-gest1-plugin-4.0.9 to package/gstreamer1. This also requires
      a modification in package/gstreamer1/Config.in.
    
    The imx-gst1-plugin package has a generic problem in Buildroot. Buildroot
    assumes that all libraries have a modified sysroot directory. Therefore
    the pkgconf tool is replaced by a wrapper pkg_config that sets (among
    others) the variable PKG_CONFIG_SYSROOT_DIR. This means all calls to
    pkgconf will return library paths with the sysroot path prepended.
    This is OK as far as the compilation and linking on the PC is affected,
    but when configuration files for the target are created, the paths must
    be set without the sysroot part. So distinguish between paths that need
    sysroot and paths that do not.
    
    This is done by adding an overall PKG_CONFIG_SYSROOT_DIR to the
    configuration environment, but removing this part when creating the
    paths for the configuration files (*.cf) by setting PKG_CONFIG_SYSROOT_DIR
    to the empty string when calling pkgconf in these cases.
    
    The last part implies a patch in package pkgconf itself. The pkg_config
    wrapper must only insert the default sysroot path if the variable
    PKG_CONFIG_SYSROOT_DIR is not set at all, but not if it is set to an
    empty string.
    
    - Fix paths that are created in configuration files in
      output/target/usr/share/aiur_registry_1.0.arm11.cf and
      output/target/usr/share/beep_registry_1.0.arm12.cf
    - This also requires a modification in package/pkgconf/pkg-config.in.

---
+++ package/gstreamer1/imx-gst1-plugin/imx-gst1-plugin.mk
@@ -0,0 +1,47 @@
+################################################################################
+#
+# imx-gst1-plugin
+#
+################################################################################
+
+IMX_GST1_PLUGIN_VERSION = 4.0.9
+IMX_GST1_PLUGIN_SITE = $(FREESCALE_IMX_SITE)
+IMX_GST1_PLUGIN_SOURCE = imx-gst1.0-plugin-$(IMX_GST1_PLUGIN_VERSION).tar.gz
+
+# Most is LGPLv2+, but some sources are copied from upstream and are
+# LGPLv2.1+, which essentially makes it LGPLv2.1+
+IMX_GST1_PLUGIN_LICENSE = LGPLv2+, LGPLv2.1+, PROPRIETARY (asf.h)
+IMX_GST1_PLUGIN_LICENSE_FILES = COPYING-LGPL-2.1 COPYING-LGPL-2
+
+IMX_GST1_PLUGIN_INSTALL_STAGING = YES
+IMX_GST1_PLUGIN_AUTORECONF = YES
+
+IMX_GST1_PLUGIN_DEPENDENCIES += host-pkgconf gstreamer1 gst1-plugins-base \
+	imx-vpuwrap imx-lib imx-vpu imx-parser imx-codec
+
+IMX_GST1_PLUGIN_CONF_ENV = \
+	PLATFORM=$(BR2_PACKAGE_IMX_GST1_PLUGIN_PLATFORM) \
+	CROSS_ROOT="$(STAGING_DIR)"
+
+# needs access to imx-specific kernel headers
+IMX_GST1_PLUGIN_DEPENDENCIES += linux
+IMX_GST1_PLUGIN_CONF_ENV += CPPFLAGS="$(TARGET_CPPFLAGS) -idirafter $(LINUX_DIR)/include/uapi -I$(@D)/libs"
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBX11),y)
+IMX_GST1_PLUGIN_DEPENDENCIES += xlib_libX11
+IMX_GST1_PLUGIN_CONF_OPTS += --enable-x11
+else
+IMX_GST1_PLUGIN_CONF_OPTS += --disable-x11
+endif
+IMX_GST1_PLUGIN_CONF_OPTS += --disable-mp3enc
+
+# Autoreconf requires an m4 directory to exist
+define IMX_GST1_PLUGIN_PATCH_M4
+	mkdir -p $(@D)/m4
+endef
+
+IMX_GST1_PLUGIN_POST_PATCH_HOOKS += IMX_GST1_PLUGIN_PATCH_M4
+
+IMX_GST1_PLUGIN_CONF_ENV += PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)"
+
+$(eval $(autotools-package))

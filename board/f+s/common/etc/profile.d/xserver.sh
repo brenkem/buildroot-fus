e9750ce361d1b0117575875539bea507b8a07ac9
Hartmut Keller <keller@fs-net.de>
2016-08-19 11:49:41 +0200

    Move .Xauthority file to /var/run
    Setting XAUTHORITY to /.Xauthority is no good idea, because the
    root filesystem may be mounted read-only. Therefore move the file
    to a place where we can always write. /var/run seems to be a good
    place since this is a ramdisk.

--- board/f+s/common/etc/profile.d/xserver.sh
+++ board/f+s/common/etc/profile.d/xserver.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # XServer setup to be run from /etc/profile.d
+#
+# Set XAUTHOROTY to a directory with write access. /var/run is a ramdisk
+# and always writable, even if the rootfs is mounted read-only.
 
 export DISPLAY=:0
-export XAUTHORITY=/.Xauthority
+export XAUTHORITY=/var/run/.Xauthority

22f27e8578c9dc5fdeb22c69828d513497713a6f
Patrick Helsen <helsen@fs-net.de>
2018-11-12 11:43:14 +0100

    Fix xserver startup issue in /etc/init.d/
    With buildroot 2018.05 a startup script S40xorg was added to
    xserver package. During the build xserver installs it to /etc/init.d/.
    Additionally our script S35x11 is installed to /etc/init.d/ as well.
    Having two startup scripts in parallel for the same task is inconsistent.
    
    S40xorg script:
    The new S40xorg script doesn't start correctly in our setup (S35x11
    script was removed). The following message is logged in /var/log/Xorg.0.log
    (WW) xf86OpenConsole: setpgid failed: Operation not permitted
    
    S35x11 script:
    S35x11 script (S40xorg removed) works fine. Furthermore with S35x11
    matchbox windows manager is started. S40xorg doesn't have a
    configuration for that.
    
    Solution:
    Keep Xserver with its S40xorg script as is, but rename custom S35x11 script to
    S40xorg. With finalscript the default S40xorg script (provided by xserver
    package) is overriden with our custom variant.

--- board/f+s/common/etc/profile.d/tslib.sh
+++ board/f+s/common/etc/profile.d/tslib.sh
@@ -3,7 +3,7 @@
 
 tsconf_file=/var/run/tslib.conf
 
-# If there is the configuration file, source it (see /etc/init.d/S35x11).
+# If there is the configuration file, source it (see /etc/init.d/S40xorg).
 # This makes ts_calibrate and ts_test work right away without the need to set
 # anything manually
 if [ -f $tsconf_file ]

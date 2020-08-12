8a23f785c32652858ae28eae21477f4747f78baf
Anatol Derksen <derksen@fs-net.de>
2020-08-12 15:37:51 +0200

    Bump kobs-ng to support iMX8M(M,N)
    Bump kobs-ng to support iMX8M(M,N)

--- package/freescale-imx/imx-kobs/imx-kobs.mk
+++ package/freescale-imx/imx-kobs/imx-kobs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IMX_KOBS_VERSION = c50b0a001d506a8c39c206b26ec24e71fcf2afb6
+IMX_KOBS_VERSION = 5d862669545a850c961b301884ff16f3828e6ce9
 IMX_KOBS_SITE = $(call github,codeauroraforum,imx-kobs,$(IMX_KOBS_VERSION))
 IMX_KOBS_LICENSE = GPL-2.0+
 IMX_KOBS_LICENSE_FILES = COPYING

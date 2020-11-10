fb77799b65e77065f4a7ee8adda0e227c8a12854
Goetz Grimmer <grimmer@fs-net.de>
2020-11-10 11:34:36 +0000

    Add FS Updater package
    Add custom FS Updater on base of rauc and Microsoft Azure IoT.

---
+++ package/FS_Update_Solution/FS_Update_Solution.mk
@@ -0,0 +1,9 @@
+###############################################################
+#
+# F&S Update Solution
+#
+###############################################################
+
+BR2_PACKAGE_FS_UPDATE_SOLUTION_VERSION = 1.0
+BR2_PACKAGE_FS_UPDATE_SOLUTION_INSTALL_TARGET = NO
+

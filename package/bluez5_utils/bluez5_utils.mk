6da327b2f50fcd561f0e4175d5fe28265badb9c4
Philipp Gerbach <gerbach@fs-net.de>
2021-06-15 09:09:05 +0000

    Update bluez_utils package
    bluez_utils was backportet to 5.39 to be compatible with the silex
    driver.
    As fsimx8mm does not use the silex chip we update to the current version
    and remove the silex specific packages.
    
    We will have to check for new patches when updating architectures which
    are using the silex chip

--- package/bluez5_utils/bluez5_utils.mk
+++ package/bluez5_utils/bluez5_utils.mk
@@ -4,15 +4,20 @@
 #
 ################################################################################
 
-BLUEZ5_UTILS_VERSION = 5.39
+# Keep the version and patches in sync with bluez5_utils-headers
+BLUEZ5_UTILS_VERSION = 5.55
 BLUEZ5_UTILS_SOURCE = bluez-$(BLUEZ5_UTILS_VERSION).tar.xz
 BLUEZ5_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
 BLUEZ5_UTILS_INSTALL_STAGING = YES
-BLUEZ5_UTILS_DEPENDENCIES = dbus libglib2
-BLUEZ5_UTILS_LICENSE = GPLv2+, LGPLv2.1+
+BLUEZ5_UTILS_LICENSE = GPL-2.0+, LGPL-2.1+
 BLUEZ5_UTILS_LICENSE_FILES = COPYING COPYING.LIB
+BLUEZ5_UTILS_CPE_ID_VENDOR = bluez
+BLUEZ5_UTILS_CPE_ID_PRODUCT = bluez
 
-BLUEZ5_UTILS_AUTORECONF = YES
+BLUEZ5_UTILS_DEPENDENCIES = \
+	$(if $(BR2_PACKAGE_BLUEZ5_UTILS_HEADERS),bluez5_utils-headers) \
+	dbus \
+	libglib2
 
 BLUEZ5_UTILS_CONF_OPTS = \
 	--enable-tools \
@@ -42,14 +47,22 @@ BLUEZ5_UTILS_CONF_OPTS += --disable-experimental
 endif
 
 # enable health plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_HEALTH),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_HEALTH),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-health
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-health
 endif
 
+# enable mesh profile
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_MESH),y)
+BLUEZ5_UTILS_CONF_OPTS += --enable-external-ell --enable-mesh
+BLUEZ5_UTILS_DEPENDENCIES += ell json-c readline
+else
+BLUEZ5_UTILS_CONF_OPTS += --disable-external-ell --disable-mesh
+endif
+
 # enable midi profile
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_MIDI),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_MIDI),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-midi
 BLUEZ5_UTILS_DEPENDENCIES += alsa-lib
 else
@@ -57,28 +70,28 @@ BLUEZ5_UTILS_CONF_OPTS += --disable-midi
 endif
 
 # enable nfc plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_NFC),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_NFC),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-nfc
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-nfc
 endif
 
 # enable sap plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_SAP),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_SAP),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-sap
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-sap
 endif
 
 # enable sixaxis plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_SIXAXIS),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_SIXAXIS),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-sixaxis
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-sixaxis
 endif
 
 # install gatttool (For some reason upstream choose not to do it by default)
-ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_GATTTOOL),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_DEPRECATED),y)
 define BLUEZ5_UTILS_INSTALL_GATTTOOL
 	$(INSTALL) -D -m 0755 $(@D)/attrib/gatttool $(TARGET_DIR)/usr/bin/gatttool
 endef
@@ -99,6 +112,13 @@ else
 BLUEZ5_UTILS_CONF_OPTS += --disable-test
 endif
 
+# enable hid2hci tool
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_TOOLS_HID2HCI),y)
+BLUEZ5_UTILS_CONF_OPTS += --enable-hid2hci
+else
+BLUEZ5_UTILS_CONF_OPTS += --disable-hid2hci
+endif
+
 # use udev if available
 ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-udev
@@ -115,10 +135,9 @@ else
 BLUEZ5_UTILS_CONF_OPTS += --disable-systemd
 endif
 
-define BLUEZ5_UTILS_INSTALL_INIT_SYSTEMD
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/
-	ln -fs ../../../../usr/lib/systemd/system/bluetooth.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/bluetooth.service
+define BLUEZ5_UTILS_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/bluez5_utils/S40bluetooth \
+		$(TARGET_DIR)/etc/init.d/S40bluetooth
 endef
 
 $(eval $(autotools-package))

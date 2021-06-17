80b53a74b399977bac0b7995fcedf5b84e624a8e
Philipp Gerbach <gerbach@fs-net.de>
2021-06-17 08:26:38 +0000

    Improve sysimg (sdcard image) generation
    Introduce new layout:
    Offset 2M: Uboot (Size 6M)
    Offset 8M: boot (Kernel + DTS) (Size 88M)
    Offset 96M: Rootfs
    
    Remove imx-ubbot specific configs

--- board/f+s/common/post-image.sh
+++ board/f+s/common/post-image.sh
@@ -53,31 +53,10 @@ genimage_type()
 	fi
 }
 
-imx_offset()
-{
-	if grep -Eq "^BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8M=y$" ${BR2_CONFIG}; then
-		echo "33K"
-	elif grep -Eq "^BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8MM=y$" ${BR2_CONFIG}; then
-		echo "33K"
-	else
-		echo "32K"
-	fi
-}
-
-uboot_image()
-{
-	if grep -Eq "^BR2_TARGET_UBOOT_FORMAT_DTB_IMX=y$" ${BR2_CONFIG}; then
-		echo "u-boot-dtb.imx"
-	elif grep -Eq "^BR2_TARGET_UBOOT_FORMAT_IMX=y$" ${BR2_CONFIG}; then
-		echo "u-boot.imx"
-	fi
-}
-
 main()
 {
 	local FILES="$(dtb_list) $(linux_image)"
-	local IMXOFFSET="$(imx_offset)"
-	local UBOOTBIN="$(uboot_image)"
+	local UBOOTBIN="u-boot-dtb.img"
 	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
 	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 

b6b77f7b5613e5690dde945b288448093601e56f
Philipp Gerbach <gerbach@fs-net.de>
2021-08-26 11:18:14 +0200

    Add xwayland defconfig
    - Add xwayland defconfig
    - activate xwayland in weston.ini
    - Unset DISPLAY environment variable  when starting weston or else
      weston will try to start with X11 backend
    - Fix glmark2 to not build x11 and drm backend when xwayland is
      activated ( This does not work)
    - Fix xorg-server to not copy S40xorg if xwayland is activated

--- package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
+++ package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
@@ -189,10 +189,12 @@ endef
 
 # init script conflicts with S90nodm
 ifneq ($(BR2_PACKAGE_NODM),y)
+ifneq ($(BR2_PACKAGE_WESTON_XWAYLAND),y)
 define XSERVER_XORG_SERVER_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 755 package/x11r7/xserver_xorg-server/S40xorg \
 		$(TARGET_DIR)/etc/init.d/S40xorg
 endef
+endif
 endif
 
 $(eval $(autotools-package))

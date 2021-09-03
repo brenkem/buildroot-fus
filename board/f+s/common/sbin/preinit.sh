8636f9273e2da8514a8fa6ef70173d6374b80927
Goetz Grimmer <grimmer@fs-net.de>
2021-09-03 13:08:17 +0000

    Adapt: Dynamic-Overlay
    Adaption for the new Dynamic-Overlay C++ adaption

--- board/f+s/common/sbin/preinit.sh
+++ board/f+s/common/sbin/preinit.sh
@@ -1,24 +1,6 @@
 #!/bin/busybox sh
 
-# Mount the /proc and /sys and /tmpfs filesystems.
-mount -t proc none /proc
-mount -t sysfs none /sys
-mount -t tmpfs tmpfs /var
-mkdir /var/lock
-
-# Do your stuff here.
-echo "This script mounts the overlay related to application in the persistent memory"
-
-# Mount the root and persistent filesystem.
-mount -t ext4 -o defaults /dev/mmcblk2p9 /rw_fs/root
-mount -t ubifs -o defaults /dev/ubi0_2 /rw_fs/root
-/usr/bin/python3 /rw_fs/dynamic_mounting.py
-
-# Clean up.
-rm -r /var/lock
-umount /var
-umount /proc
-umount /sys
+/sbin/dynamic_overlay
 
 
 # Boot the real thing.

eb80082f04077383d7053fbfd6aec70d2739379e
Goetz Grimmer <grimmer@fs-net.de>
2021-09-03 12:06:45 +0000

    Replace: Former FSUF concept (python3) with C++ impl. external tree

--- package/fs-update-solution/fs-update-solution.mk
+++
@@ -1,9 +0,0 @@
-###############################################################
-#
-# F&S Update Solution
-#
-###############################################################
-
-BR2_PACKAGE_FS_UPDATE_SOLUTION_VERSION = 1.0
-BR2_PACKAGE_FS_UPDATE_SOLUTION_INSTALL_TARGET = NO
-

1e602f05280f39925a7457d333792c8e9c34f390
Philipp Gerbach <gerbach@fs-net.de>
2021-10-04 16:31:11 +0200

    Add bulding of install.scr script with buildroot
    Until now the install.scr script was built externally with a own mkimage
    tool.
    
    For better reproducibility, we want to build the install.txt with
    Buildroot/Yocto.
    
    We use the mkimage tool from the host-uboot-tools package, which might
    have a different version than the uboot we build but should still work.
    
    The install script is executed in finalscript_min-common so it will be
    applied for every build. By default the source at
    board/f+s/common/install.txt is used.
    
    If one wants to use a different source, they can st the variable
    $INSTALL_TXT_PATH in the board specific final script to the respective
    source.
    
    Also remove the imx8mm specific version, because it should work with the
    common one.

---
+++ board/f+s/common/mkinstall.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INSTALL_TXT_PATH=$1
+
+if [ -z "$var" ];then
+	INSTALL_TXT_PATH=board/f+s/common
+fi
+
+install_src="$INSTALL_TXT_PATH/install.txt"
+OUTPUT=output/images
+install_bin="$OUTPUT/install.scr"
+MKIMAGE=mkimage
+
+if grep -Eq "^BR2_aarch64=y$" ${BR2_CONFIG}; then
+	ARCH=arm64
+else
+	ARCH=arm
+fi
+
+$MKIMAGE -V
+echo $ARCH
+$MKIMAGE -A $ARCH -O u-boot -T script -C none -n "F&S install script" -d "$install_src" "$install_bin"

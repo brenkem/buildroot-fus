b68ef1cdf63e81f942876837b61e861429b3f750
Hartmut Keller <keller@fs-net.de>
2022-05-17 18:49:21 +0200

    Update package silex-wlanbt-fs to v4.5.25.38-0
    The new driver version is based on version 4.5.25.38 of the original
    Qualcomm driver which is available in branch CNSS.LEA.NRT_3.1 on
    repository (tag v4.5.25.38)
    
      http://source.codeaurora.org/external/wlan/qcacld-2.0
    
    But Silex has added a whole lot of commits that were done later in
    the development, up to version 4.5.25.52. And Silex has also added
    some own changes.
    
    Through a detailed analysis of the changes we were able to define
    a complete list of patches that Silex has taken from the original
    code. Then we added one final patch that brings in all Silex specific
    changes. This results in the 56 patches that are now here in the
    F&S package. Of course all previous patches are obsolete now.
    
    So download the right version from the codeaurora git repository,
    and apply alle the patches. This results in the Silex version. The
    full official Silex version is "4.5.25.38 sxa 1.0.0.019".

--- package/silex-wlanbt-fs/silex-wlanbt-fs.mk
+++ package/silex-wlanbt-fs/silex-wlanbt-fs.mk
@@ -6,28 +6,28 @@
 #
 ################################################################################
 
-SILEX_WLANBT_FS_VERSION = 4.0.11.213V
+SILEX_WLANBT_FS_VERSION = v4.5.25.38
 SILEX_WLANBT_FS_SITE = http://source.codeaurora.org/external/wlan/qcacld-2.0/snapshot
 SILEX_WLANBT_FS_SOURCE = $(SILEX_WLANBT_FS_VERSION).tar.gz
 #SILEX_WLANBT_FS_SITE_METHOD = wget
 SILEX_WLANBT_FS_LICENSE = GPL-2.0 or BSD
 
-# Options (taken from LEA.3.0, config.te-f30) necessary for selecting the
-# correct defines in Kbuild.
+# Options (taken from Silex driver LEA.3.0 4.5.25.38 sxa 1.0.0.019)
+# necessary for selecting the correct defines in Kbuild.
 #
 # Attention: These settings will override any assignments for these variables
-# done in Kbuild; for example if CONFIG_PER_VDEV_TX_DESC_POOL is set, the
-# assignment := 0 in Kbuild in case of SDIO interface is ignored even if the
-# condition for it there is true. This is why we do not set it here.
+# done in qcacld-2.0/Kbuild; if a variable is set here in one way, but
+# to a different value in Kbuild, the assignment in Kbuild is ignored.
+# So make sure that you do not set values that contradict Kbuild
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS = KERNEL_SRC=$(LINUX_DIR)
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += WLAN_ROOT=$(@D)
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += MODNAME=wlan
-SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_WIFI_ISOC=0
-SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_WIFI_2_0=1
-SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_CLD_WLAN=m
-SILEX_WLANBT_FS_MODULE_MAKE_OPTS += WLAN_OPEN_SOURCE=1
-SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_CLD_HL_SDIO_CORE=y
+# Options taken from build/scripts/imx6sx/config.imx6sx
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_LINUX_QCMBR=y
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_NON_QC_PLATFORM=y
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_WLAN_THERMAL_SHUTDOWN=0
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_CLD_HL_SDIO_CORE=y
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_PER_VDEV_TX_DESC_POOL=1
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += SAP_AUTH_OFFLOAD=1
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_LL_TX_FLOW_CT=1
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_WLAN_FEATURE_FILS=y
@@ -35,8 +35,14 @@ SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_FEATURE_COEX_PTA_CONFIG_ENABLE=y
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_SUPPORT_TXRX_DRIVER_TCP_DEL_ACK=y
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_WLAN_WAPI_MODE_11AC_DISABLE=y
 SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_WLAN_WOW_PULSE=y
-# Set in config.te-f30, but according to Kbuild not available in SDIO mode
-#SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_PER_VDEV_TX_DESC_POOL=1
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_WLAN_FEATURE_11W=y
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_TXRX_PERF=y
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_PMF_SUPPORT=y
+# Options taken from qcacld-2.0/Makefile
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_CLD_WLAN=m
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_WIFI_ISOC=0
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += CONFIG_QCA_WIFI_2_0=1
+SILEX_WLANBT_FS_MODULE_MAKE_OPTS += WLAN_OPEN_SOURCE=1
 
 $(eval $(kernel-module))
 

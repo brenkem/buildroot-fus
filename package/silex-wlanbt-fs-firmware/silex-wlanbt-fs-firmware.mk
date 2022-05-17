ce29084d658a0826fa6e607cb4d88b25a42e326d
Hartmut Keller <keller@fs-net.de>
2022-05-17 19:05:35 +0200

    Update silex-wlanbt-fs-firmware to v4.5.25.38-0
    The new Silex driver also needs a new set of firmware files.
    
    /lib/firmware/WLAN-firmware/bin/bdwlan30.bin:
       F&S sepcific board config, but a version slightly updated by Silex
       from May 2022. The changes modify the radio calibration for better
       Tx power control accuracy. Max Tx power levels remain the same.
    /lib/firmware/WLAN-firmware/bin/qwlan30.bin:
       New firmware from the Qualcomm driver (firmware/WLAN-firmware-lea2/bin)
    /lib/firmware/WLAN-firmware/bin/otp30.bin:
       Unchanged since 4.0.11.213V (firmware/WLAN-firmware/otp30.bin)
    /lib/firmware/utf30.bin:
       New version from Silex from May 2022 (firmware/WLAN-firmware/bin)
    /lib/firmware/utfbd30.bin:
       Same as bdwlan30.bin (symbolic link)
    /lib/firmware/wlan/qcom_cfg.ini:
       Neues Silex ini file, slightly modified from original Qualcomm
       version (qcacld-new/firmware_bin/qcom_cfg.ini)
    /lib/firmware/wlan/cfg.dat:
       New version from the Qualcomm driver (renamed from
       qcacld-new/firmware_bin/WCNSS_cfg.dat)
    /lib/firmware/BT-firmware/BD-SDMAC/nvm_tlv_tf_1.1.bin:
       F&S specific version, unchanged since 4.0.11.213V
    /lib/firmware/qca/rampatch_le_2-3-3_tlv_tf_1.1_bt4-2.tlv
       Like before, support for Bluetooth-4.2
    /lib/firmware/qca/rampatch_tlv_tf_1.1.tlv:
       Same as above (symbolic link)
    
    The firmware files themselves are provided in the new tar file
    
      silex-wlanbt-fs-firmware-v4.5.25.38-0.tar.bz2
    
    that has to be added to all releases where the Silex chip is part of. So
    this commit only changes the makefile silex-wlanbt-fs-firmware.mk.

--- package/silex-wlanbt-fs-firmware/silex-wlanbt-fs-firmware.mk
+++ package/silex-wlanbt-fs-firmware/silex-wlanbt-fs-firmware.mk
@@ -6,14 +6,16 @@
 #
 ################################################################################
 
-SILEX_WLANBT_FS_FIRMWARE_VERSION = 4.0.11.213V-0
+SILEX_WLANBT_FS_FIRMWARE_VERSION = v4.5.25.38-0
 SILEX_WLANBT_FS_FIRMWARE_SITE = $(TOPDIR)/package/silex-wlanbt-fs-firmware
 SILEX_WLANBT_FS_FIRMWARE_SOURCE = silex-wlanbt-fs-firmware-$(SILEX_WLANBT_FS_FIRMWARE_VERSION).tar.bz2
 SILEX_WLANBT_FS_FIRMWARE_SITE_METHOD = file
 SILEX_WLANBT_FS_FIRMWARE_LICENSE = Proprietary
+#SILEX_WLANBT_FS_FIRMWARE_DEPENDENCIES = silex-wlanbt-fs
 
 # Also add firmware files
-SILEX_WLANBT_FS_FIRMWARE_FIRMWARE_FILES = bdwlan30.bin otp30.bin qwlan30.bin utf30.bin utfbd30.bin Notice.txt
+SILEX_WLANBT_FS_FIRMWARE_FIRMWARE_FILES = bdwlan30.bin otp30.bin qwlan30.bin \
+	utf30.bin utfbd30.bin COPYRIGHT
 SILEX_WLANBT_FS_FIRMWARE_FIRMWARE_DIRS = qca wlan
 
 # Subcommand to install single firmware files

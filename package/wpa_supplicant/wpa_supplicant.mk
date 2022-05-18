06d149c47399496b5ee355c4d564668134a8a56f
Hartmut Keller <keller@fs-net.de>
2022-05-18 15:25:02 +0200

    Update hostapd and wpa_supplicant to a Silex specific 2.10-devel version
    Thw two packages hostapd and wpa_supplicant are based on the same codebase
    which is available as hostap GIT repository. (git://w1.fi/srv/git/hostap.git)
    
    Silex uses an early devel version of hostap-2.10 that was forked at or
    arround commit 100b2edb2806c35de8f941859e212b86d1658382. An additional patch
    is provided in the Silex driver repository.
    
    Instead of using the release packages, that are available as two separate tar
    files for hostapd and wpa_supplicant, download the above GIT version directly,
    apply the above patch and an additional patch that contains all other changes
    that Silex has made. This results in the Silex specific version. It has the
    official version "2.10-devel sxa 1.0.0.a19" (see src/common/version.h in the
    resulting source tree).
    
    We download the same code in both packages, wich means we also have to
    provide the same two patches in both packages. All previous patches are
    obsolete now.
    
    Also change the set of CONFIG options in Config.in. The new version supports
    WPA3 now and has some different settings. This also requires some changes
    in the makefiles of the two packages.

--- package/wpa_supplicant/wpa_supplicant.mk
+++ package/wpa_supplicant/wpa_supplicant.mk
@@ -4,25 +4,18 @@
 #
 ################################################################################
 
-WPA_SUPPLICANT_VERSION = 2.7
-WPA_SUPPLICANT_SITE = http://w1.fi/releases
-WPA_SUPPLICANT_PATCH = \
-	https://w1.fi/security/2019-1/0001-OpenSSL-Use-constant-time-operations-for-private-big.patch \
-	https://w1.fi/security/2019-1/0002-Add-helper-functions-for-constant-time-operations.patch \
-	https://w1.fi/security/2019-1/0003-OpenSSL-Use-constant-time-selection-for-crypto_bignu.patch \
-	https://w1.fi/security/2019-2/0004-EAP-pwd-Use-constant-time-and-memory-access-for-find.patch \
-	https://w1.fi/security/2019-1/0005-SAE-Minimize-timing-differences-in-PWE-derivation.patch \
-	https://w1.fi/security/2019-1/0006-SAE-Avoid-branches-in-is_quadratic_residue_blind.patch \
-	https://w1.fi/security/2019-1/0007-SAE-Mask-timing-of-MODP-groups-22-23-24.patch \
-	https://w1.fi/security/2019-1/0008-SAE-Use-const_time-selection-for-PWE-in-FFC.patch \
-	https://w1.fi/security/2019-1/0009-SAE-Use-constant-time-operations-in-sae_test_pwd_see.patch \
-	https://w1.fi/security/2019-3/0010-SAE-Fix-confirm-message-validation-in-error-cases.patch \
-	https://w1.fi/security/2019-4/0011-EAP-pwd-server-Verify-received-scalar-and-element.patch \
-	https://w1.fi/security/2019-4/0012-EAP-pwd-server-Detect-reflection-attacks.patch \
-	https://w1.fi/security/2019-4/0013-EAP-pwd-client-Verify-received-scalar-and-element.patch \
-	https://w1.fi/security/2019-4/0014-EAP-pwd-Check-element-x-y-coordinates-explicitly.patch \
-	https://w1.fi/security/2019-5/0001-EAP-pwd-server-Fix-reassembly-buffer-handling.patch \
-	https://w1.fi/security/2019-5/0003-EAP-pwd-peer-Fix-reassembly-buffer-handling.patch
+# 28.04.2022 F&S:
+# wpa_supplicant is part of project hostap that also contains hostapd. There
+# are separate release tar files for hostapd and wpa_suppliant, but there is
+# only one git. The Silex version "2.10-devel sxa 1.0.0.a19" is based on an
+# early developer version of 2.10 and was most probably branched off at commit
+# 100b2edb2806c35de8f941859e212b86d1658382. Download this version and apply the
+# remaining Silex modifications, e.g. with a different defconfig for building.
+WPA_SUPPLICANT_VERSION = 100b2edb2806c35de8f941859e212b86d1658382
+WPA_SUPPLICANT_SITE = git://w1.fi/srv/git/hostap.git
+WPA_SUPPLICANT_METHOD = git
+#WPA_SUPPLICANT_VERSION = 2.9
+#WPA_SUPPLICANT_SITE = http://w1.fi/releases
 WPA_SUPPLICANT_LICENSE = BSD-3-Clause
 WPA_SUPPLICANT_LICENSE_FILES = README
 WPA_SUPPLICANT_CONFIG = $(WPA_SUPPLICANT_DIR)/wpa_supplicant/.config
@@ -45,10 +38,10 @@ WPA_SUPPLICANT_CONFIG_ENABLE = \
 	CONFIG_IEEE80211N \
 	CONFIG_IEEE80211R \
 	CONFIG_INTERNAL_LIBTOMMATH \
-	CONFIG_DEBUG_FILE \
 	CONFIG_MATCH_IFACE
 
 WPA_SUPPLICANT_CONFIG_DISABLE = \
+	CONFIG_DEBUG_FILE \
 	CONFIG_SMARTCARD
 
 # libnl-3 needs -lm (for rint) and -lpthread if linking statically
@@ -84,10 +77,16 @@ ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_AP_SUPPORT),y)
 WPA_SUPPLICANT_CONFIG_ENABLE += \
 	CONFIG_AP \
 	CONFIG_P2P
+else
+WPA_SUPPLICANT_CONFIG_DISABLE += \
+	CONFIG_AP \
+	CONFIG_P2P
 endif
 
 ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_WIFI_DISPLAY),y)
 WPA_SUPPLICANT_CONFIG_ENABLE += CONFIG_WIFI_DISPLAY
+else
+WPA_SUPPLICANT_CONFIG_DISABLE += CONFIG_WIFI_DISPLAY
 endif
 
 ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_MESH_NETWORKING),y)
@@ -105,6 +104,18 @@ ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_WPS),y)
 WPA_SUPPLICANT_CONFIG_ENABLE += CONFIG_WPS
 endif
 
+ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_WPA3),y)
+WPA_SUPPLICANT_CONFIG_ENABLE += \
+	CONFIG_DPP \
+	CONFIG_SAE \
+	CONFIG_OWE
+else
+WPA_SUPPLICANT_CONFIG_DISABLE += \
+	CONFIG_DPP \
+	CONFIG_SAE \
+	CONFIG_OWE
+endif
+
 # Try to use openssl if it's already available
 ifeq ($(BR2_PACKAGE_LIBOPENSSL),y)
 WPA_SUPPLICANT_DEPENDENCIES += host-pkgconf libopenssl
@@ -115,34 +126,24 @@ WPA_SUPPLICANT_CONFIG_DISABLE += CONFIG_EAP_PWD
 WPA_SUPPLICANT_CONFIG_EDITS += 's/\#\(CONFIG_TLS=\).*/\1internal/'
 endif
 
-ifeq ($(BR2_PACKAGE_DBUS),y)
+ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_DBUS),y)
 WPA_SUPPLICANT_DEPENDENCIES += host-pkgconf dbus
 WPA_SUPPLICANT_MAKE_ENV = \
 	PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
 	PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
-
-ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_DBUS_OLD),y)
-WPA_SUPPLICANT_CONFIG_ENABLE += CONFIG_CTRL_IFACE_DBUS=
-define WPA_SUPPLICANT_INSTALL_DBUS_OLD
-	$(INSTALL) -m 0644 -D \
-		$(@D)/wpa_supplicant/dbus/$(WPA_SUPPLICANT_DBUS_OLD_SERVICE).service \
-		$(TARGET_DIR)/usr/share/dbus-1/system-services/$(WPA_SUPPLICANT_DBUS_OLD_SERVICE).service
-endef
-endif
-
-ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_DBUS_NEW),y)
 WPA_SUPPLICANT_CONFIG_ENABLE += CONFIG_CTRL_IFACE_DBUS_NEW
 define WPA_SUPPLICANT_INSTALL_DBUS_NEW
 	$(INSTALL) -m 0644 -D \
 		$(@D)/wpa_supplicant/dbus/$(WPA_SUPPLICANT_DBUS_NEW_SERVICE).service \
 		$(TARGET_DIR)/usr/share/dbus-1/system-services/$(WPA_SUPPLICANT_DBUS_NEW_SERVICE).service
 endef
-endif
 
 ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_DBUS_INTROSPECTION),y)
 WPA_SUPPLICANT_CONFIG_ENABLE += CONFIG_CTRL_IFACE_DBUS_INTRO
 endif
 
+else
+WPA_SUPPLICANT_CONFIG_DISABLE += CONFIG_CTRL_IFACE_DBUS_NEW
 endif
 
 ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_DEBUG_SYSLOG),y)

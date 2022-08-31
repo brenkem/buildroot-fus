51f8a5e0c043f9375e6cbd58e75128b0e9274b2d
Kay Mueller <mueller@fs-net.de>
2022-08-31 18:52:45 +0200

    Add support for eMMC sysimg

--- board/f+s/fsimx6sx/local-fsimx6sx.mk
+++ board/f+s/fsimx6sx/local-fsimx6sx.mk
@@ -1,1 +1,3 @@
 LINUX_OVERRIDE_SRCDIR = ../linux-fsimx6sx/
+UBOOT_OVERRIDE_SRCDIR = ../u-boot-fsimx6sx/
+
--- board/f+s/fsimx6ul/local-fsimx6ul.mk
+++ board/f+s/fsimx6ul/local-fsimx6ul.mk
@@ -1,1 +1,3 @@
 LINUX_OVERRIDE_SRCDIR = ../linux-fsimx6ul/
+UBOOT_OVERRIDE_SRCDIR = ../u-boot-fsimx6sx/
+

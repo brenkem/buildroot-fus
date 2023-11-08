62264aa5fd5cf4360b46c4b36a5b68fdb665c558
Philipp Gerbach <gerbach@fs-net.de>
2023-11-08 13:50:41 +0100

    Add F&S psplash boot screen
    Add an sysv implementation of the pslash boot screen with an F&S logo.
    Customers can change BR2_PACKAGE_PSPLASH_IMAGE in the defconfig to set
    their own logo.
    Also they can configer psplash by altering
    package/psplash/psplash-colors.h and psplash-config.h

--- package/psplash/psplash.mk
+++ package/psplash/psplash.mk
@@ -21,12 +21,20 @@ endif
 
 PSPLASH_IMAGE = $(call qstrip,$(BR2_PACKAGE_PSPLASH_IMAGE))
 
+ifeq ($(BR2_PACKAGE_CONFIG_PSPLASH),y)
+define PSPLASH_COPY_CONFIG
+	cp -f package/psplash/psplash-config.h $(@D)/
+	cp -f package/psplash/psplash-colors.h $(@D)/
+	cp -f package/psplash/radeon-font.h $(@D)/
+endef
+endif
+
 ifneq ($(PSPLASH_IMAGE),)
 define PSPLASH_COPY_IMAGE
 	cp $(PSPLASH_IMAGE) $(@D)/base-images/psplash-poky.png
 endef
 
-PSPLASH_POST_EXTRACT_HOOKS += PSPLASH_COPY_IMAGE
+PSPLASH_POST_EXTRACT_HOOKS += PSPLASH_COPY_IMAGE PSPLASH_COPY_CONFIG
 endif
 
 define PSPLASH_INSTALL_INIT_SYSTEMD
@@ -35,6 +43,23 @@ define PSPLASH_INSTALL_INIT_SYSTEMD
 
 	$(INSTALL) -D -m 644 package/psplash/psplash-systemd.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/psplash-systemd.service
+endef
+
+define PSPLASH_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/psplash/SysV_scripts/S01psplashInit \
+		$(TARGET_DIR)/etc/init.d/S01psplashInit
+	$(INSTALL) -D -m 755 package/psplash/SysV_scripts/S20psplashWrite \
+		$(TARGET_DIR)/etc/init.d/S20psplashWrite
+	$(INSTALL) -D -m 755 package/psplash/SysV_scripts/S40psplashWrite \
+		$(TARGET_DIR)/etc/init.d/S40psplashWrite
+	$(INSTALL) -D -m 755 package/psplash/SysV_scripts/S60psplashWrite \
+		$(TARGET_DIR)/etc/init.d/S60psplashWrite
+	$(INSTALL) -D -m 755 package/psplash/SysV_scripts/S80psplashWrite \
+		$(TARGET_DIR)/etc/init.d/S80psplashWrite
+	$(INSTALL) -D -m 755 package/psplash/SysV_scripts/S97psplashWrite \
+		$(TARGET_DIR)/etc/init.d/S97psplashWrite
+	$(INSTALL) -D -m 755 package/psplash/SysV_scripts/S99pslpashQuit \
+		$(TARGET_DIR)/etc/init.d/S99pslpashQuit
 endef
 
 $(eval $(autotools-package))

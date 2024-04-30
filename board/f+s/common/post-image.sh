a9cdcc7b2c2a0cdc34d5df4b7a2b6c8de6e950dd
Maik Brenke <maik.brenke@continental-corporation.com>
2024-04-30 11:07:18 +0200

    adjust post-image script to support different device tree options, e.g.: BR2_LINUX_KERNEL_CUSTOM_DTS_PATH

--- board/f+s/common/post-image.sh
+++ board/f+s/common/post-image.sh
@@ -23,10 +23,10 @@ freertos_sample_list()
 #
 dtb_list()
 {
-	local DTB_LIST="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\([A-Z \+ | \. \+ | \/a-z0-9 \-]*\)"$/\1/p' ${BR2_CONFIG})"
+	local DTB_LIST="$(sed -n 's/^BR2_LINUX_KERNEL_[A-Z \+ | \. \+ | \/a-z0-9 \-]*_DTS_[A-Z \+ | \. \+ | \/a-z0-9 \-]*="\([A-Z \+ | \. \+ | \/a-z0-9 \-]*\)"$/\1/p' ${BR2_CONFIG})"
 
 	for dt in $DTB_LIST; do
-		echo -n "\"`basename $dt`.dtb\", "
+		echo -n "\"`basename ${dt%.*}`.dtb\", "
 	done
 }
 
